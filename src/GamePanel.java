c119d13657c2d10415659f7ef951646bd9e1c3c4
taiphanvan2k3 <taiphanvan2403@gmail.com>
2023-02-05 07:44:14 +0700

    Test

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -64,7 +64,7 @@ public class GamePanel extends JPanel implements ActionListener {
 				while (true) {
 					if (checkAppleEaten) {
 						try {
-							threadForRenderApple.sleep(4000);
+							threadForRenderApple.sleep(2000);
 						} catch (InterruptedException e) {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
@@ -88,7 +88,7 @@ public class GamePanel extends JPanel implements ActionListener {
 				}
 			}
 		};
-		threadForPlayingTime.start();
+		// threadForPlayingTime.start();
 		threadForRenderApple.start();
 	}
 
@@ -112,13 +112,11 @@ public class GamePanel extends JPanel implements ActionListener {
 	// Ko phải phương thức override
 	public void draw(Graphics g) {
 		if (running) {
-			// System.out.println(g.getColor());
-			for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
-				// Vẽ các đường thẳng để chia lưới panel
-				g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
-				g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
-			}
-
+			/*
+			 * for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) { // Vẽ các đường thẳng
+			 * để chia lưới panel g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE,
+			 * SCREEN_HEIGHT); g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE); }
+			 */
 			if (!checkAppleEaten) {
 				g.setColor(Color.red);
 				g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
